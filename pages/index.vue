7beab27b4c67684a2f24775341ab6d7965e49824
Ahmed Qamar Mannan <ahmed.qamar@alliedc.com>
2024-10-10 05:17:05 +0500

    Work in progress.

---
+++ pages/index.vue
@@ -0,0 +1,164 @@
+<template>
+    <v-app>
+      <v-container>
+        <!-- <v-card> -->
+          <v-row>
+            <v-col><v-date-input label="Start Date" variant="solo-inverted"></v-date-input>    
+            </v-col>
+            <v-col><v-date-input label="End Date" variant="solo-inverted"></v-date-input>    
+            </v-col>
+          </v-row>
+          <!-- <v-date-input label="Date input" variant="solo-inverted"></v-date-input> -->
+        <!-- </v-card> -->
+      </v-container>
+      <!-- <v-spacer></v-spacer> -->
+      <v-container>
+        <!-- <v-card>
+          <v-date-input label="Date input" variant="solo-inverted" max-width="200"></v-date-input>
+        </v-card> -->
+        <v-card>
+          <!-- <v-date-input label="Date input" variant="solo-inverted" max-width="200"></v-date-input> -->
+          <v-data-table-server v-model:items-per-page="itemsPerPage" :headers="headers" :items="serverItems"
+            :items-length="totalItems" :loading="loading" :search="search" item-value="name"
+            @update:options="loadItems">
+            <!-- <template v-slot:item.actions="{ item }">
+              <v-icon small @click="viewItem(item)">mdi-eye</v-icon>
+  
+            </template> -->
+            <template v-slot:item.readings="{ item }">
+              <!-- <v-icon small @click="viewItem(item)">mdi-eye</v-icon> -->
+               <!-- <p>{{item.data_readings}}</p> -->
+                <v-row>
+               <!-- <li v-for="reading in item.data_readings"> -->
+                <v-col v-for="reading in item.data_readings" outline><b>{{ reading.value}}</b></v-col>
+                
+               <!-- </li> -->
+              </v-row>
+            </template>
+          </v-data-table-server>
+        </v-card>
+      </v-container>
+    </v-app>
+  </template>
+  
+  <script>
+  
+  //   const { data, error } = await useAsyncData('fetchData', () => $fetch('/api/deviceData'));
+  
+  // console.log('server API resp : ', data)
+  
+  //const API = {
+    
+  //}
+  
+  export default {
+    data: () => ({
+      itemsPerPage: 10,
+      headers: [
+        {
+          title: 'Date',
+          align: 'start',
+          sortable: false,
+          key: 'date',
+        },
+        {
+          title: 'Device',
+          align: 'center',
+          sortable: false,
+          key: 'device.device_type.type',
+        },
+        {
+          title: 'Station',
+          align: 'center',
+          sortable: false,
+          key: 'device.station.name',
+        },
+        // {
+        //   title: 'TTL',
+        //   key: 'ttl',
+        //   align: 'center'
+        // },
+        // {
+        //   title: 'Actions',
+        //   value: 'actions',
+        //   align: 'center',
+        // }
+        {
+          title: 'Readings',
+          value: 'readings',
+          // key: 'device.station.name',
+          align: 'center',
+        }
+  
+        // { title: 'Fat (g)', key: 'fat', align: 'end' },
+        // { title: 'Carbs (g)', key: 'carbs', align: 'end' },
+        // { title: 'Protein (g)', key: 'protein', align: 'end' },
+        // { title: 'Iron (%)', key: 'iron', align: 'end' },
+      ],
+      search: '',
+      serverItems: [],
+      loading: true,
+      totalItems: 0,
+      dialog: false,
+      selectedItem: {}
+    }),
+    methods: {
+      async fetchDeviceData({ page, itemsPerPage, sortBy }) {
+      const { data } = await useFetch('/api/deviceData')
+      // console.log('resp data : ', data.value)
+      console.log('resp data : ', data)
+      console.log('resp data.value : ', data.value)
+      console.log('resp data.value.data : ', data.value.data)
+      return new Promise(resolve => {
+        setTimeout(() => {
+          const start = (page - 1) * itemsPerPage
+          const end = start + itemsPerPage
+          //       console.log('actual data : ', data.data)
+          const items = data.value.data.slice()
+  
+          //     //   if (sortBy.length) {
+          //     //     const sortKey = sortBy[0].key
+          //     //     const sortOrder = sortBy[0].order
+          //     //     items.sort((a, b) => {
+          //     //       const aValue = a[sortKey]
+          //     //       const bValue = b[sortKey]
+          //     //       return sortOrder === 'desc' ? bValue - aValue : aValue - bValue
+          //     //     })
+          //     //   }
+  
+          const paginated = items.slice(start, end)
+  
+          resolve({ items: paginated, total: items.length })
+        }, 500)
+      })
+    },
+      async loadItems({ page, itemsPerPage, sortBy }) {
+        this.loading = true
+        const data = await this.fetchDeviceData({ page, itemsPerPage, sortBy })
+        console.log('data : ', data)
+        console.log('data.items : ', data.items)
+        console.log('data.value : ', data.value)
+        console.log('data.data : ', data.data)
+        // { //.then(({ items, total }) => {
+        //   console.log("Items : ", items)
+          this.serverItems = data.items
+          // this.totalItems = total
+          this.loading = false
+        // }
+      },
+  
+      viewItem(item) {
+        console.log('View item : ', item)
+        console.log('View item.id : ', item.id)
+        // Logic to edit the item
+      },
+  
+      showDetails(item) {
+        console.log('Selected Item: ', item)
+        selectedItem.value = item
+        dialog = true
+      }
+    },
+  }
+  </script>
+  <!-- </template> -->
--- pages/login.vue
+++ pages/login.vue
@@ -1,46 +1,73 @@
 <template>
-    <!-- <NuxtLayout> -->
-  <v-app>
-    <v-container>
-    <v-card>
-      <v-card-title>
-        <h1 class="display-1">Login</h1>
-      </v-card-title>
-      <v-card-text>
-        <v-form>
-          <v-text-field label="Username" v-model="name" prepend-icon="mdi-account-circle" />
-          <v-text-field label="Password" v-model="password" type="password" prepend-icon="mdi-lock" />
-        </v-form>
-      </v-card-text>
-      <!-- <v-btn color="info">Login</v-btn> -->
-      <v-card-actions>
-        <v-btn color="info" variant="tonal">Login</v-btn>
-      </v-card-actions>
-    </v-card>
-  </v-container>
-  </v-app>    
-  <!-- </NuxtLayout> -->
-  </template>
+    <v-sheet class="mx-auto" max-width="300">
+      <v-form @submit.prevent="submit">
+        <v-text-field
+          label="Username"
+          v-model="email" prepend-icon="mdi-account-circle"
+        ></v-text-field>
+        <v-text-field label="Password" v-model="password" type="password" prepend-icon="mdi-lock" />
   
-  
-  <script>
+        <v-btn
+          :loading="loading"
+          class="mt-2"
+          text="Submit"
+          type="submit"
+          block
+        ></v-btn>
+      </v-form>
+    </v-sheet>
+</template>
+
+
+<script>
+  const auth = useCookie('auth-cookie')
   export default {
-    data() {
-      return {
-        name: '',
-        email: '',
-      };
-    },
+    data: () => ({
+    //   loading: false,
+      //rules: [value => vm.checkApi(value)],
+    //   timeout: null,
+      email: '',
+      password: '',
+    //   auth: useCookie('auth-cookie')
+    }),
+
     methods: {
-      handleSubmit() {
-        // Validate form data here
-        if (!this.name || !this.email) {
-          return;
-        }
-  
-        // Send form data to server using API call or other method
-        console.log('Form data:', this.name, this.email);
+      async submit (event) {
+
+        console.log('submit clicked : ')
+        console.log('submit email : ', this.email)
+
+        const results = await event
+
+        const { data } = await useFetch('/api/login', {
+            method: 'POST',
+            params: {
+                email: this.email,
+                password: this.password
+            }
+        })
+
+        console.log('auth-resp on client : ', data.value)
+
+        auth.value = data.value.access_token
+
+        // this.loading = false
+
+        // alert(JSON.stringify(results, null, 2))
       },
+    //   async checkApi (userName) {
+    //     // return new Promise(resolve => {
+    //     //   clearTimeout(this.timeout)
+
+    //     //   this.timeout = setTimeout(() => {
+    //     //     if (!userName) return resolve('Please enter a user name.')
+    //     //     if (userName === 'johnleider') return resolve('User name already taken. Please try another one.')
+
+    //     //     return resolve(true)
+    //     //   }, 1000)
+    //     // })
+
+    //   },
     },
-  };
-  </script>
+  }
+</script>
