ace8e436886b8c92bef0ec780b9a41662ec3da38
Ahmed Qamar Mannan <ahmed.qamar@alliedc.com>
2024-10-13 22:32:08 +0500

    Latest work before demo.

--- pages/index.vue
+++ pages/index.vue
@@ -2,12 +2,12 @@
     <v-app>
       <v-container>
         <!-- <v-card> -->
-          <v-row>
+          <!-- <v-row>
             <v-col><v-date-input label="Start Date" variant="solo-inverted"></v-date-input>    
             </v-col>
             <v-col><v-date-input label="End Date" variant="solo-inverted"></v-date-input>    
             </v-col>
-          </v-row>
+          </v-row> -->
           <!-- <v-date-input label="Date input" variant="solo-inverted"></v-date-input> -->
         <!-- </v-card> -->
       </v-container>
@@ -25,24 +25,72 @@
               <v-icon small @click="viewItem(item)">mdi-eye</v-icon>
   
             </template> -->
-            <template v-slot:item.readings="{ item }">
+            <template v-slot:item.date="{ item }">
               <!-- <v-icon small @click="viewItem(item)">mdi-eye</v-icon> -->
                <!-- <p>{{item.data_readings}}</p> -->
                 <v-row>
                <!-- <li v-for="reading in item.data_readings"> -->
-                <v-col v-for="(reading, index) in item.data_readings" outline>
-                <small v-if="index == 0">Operator ID: </small>
-                <small v-if="index == 1">Vehicle #: </small>
-                <small v-if="index == 2">Fuel Type: </small>
-                <small v-if="index == 3">Litres: </small>
-                <small v-if="index == 4">Amount: </small>
-                <small v-if="index == 5">Rate: </small>
-                <small v-if="index == 6">Total Litres: </small>
-                <small v-if="index == 7">Total Amount: </small>
-                <b>{{ reading.value}}</b>
+                <v-col outline>
+                <b>{{ item.date.split('T')[0]}} {{item.date.split('T')[1]}}</b>
                 </v-col>
                 
                <!-- </li> -->
+              </v-row>
+            </template>
+            <template v-slot:item.operators="{ item }">
+                <v-row>
+                <v-col outline>
+                {{ item.data_readings[0].value}}
+                </v-col>
+              </v-row>
+            </template>
+            <template v-slot:item.vehicles="{ item }">
+                <v-row>
+                <v-col outline>
+                {{ item.data_readings[1].value}}
+                </v-col>
+              </v-row>
+            </template>
+            <template v-slot:item.fuel="{ item }">
+                <v-row>
+                <v-col outline>
+                {{ item.data_readings[2].value}}
+                </v-col>
+              </v-row>
+            </template>
+            <template v-slot:item.litres="{ item }">
+                <v-row>
+                <v-col outline>
+                {{ item.data_readings[3].value/10}}
+                </v-col>
+              </v-row>
+            </template>
+            <template v-slot:item.amount="{ item }">
+                <v-row>
+                <v-col outline>
+                {{ item.data_readings[4].value}}
+                </v-col>
+              </v-row>
+            </template>
+            <template v-slot:item.rate="{ item }">
+                <v-row>
+                <v-col outline>
+                {{ item.data_readings[5].value/100}}
+                </v-col>
+              </v-row>
+            </template>
+            <template v-slot:item.totalLitres="{ item }">
+                <v-row>
+                <v-col outline>
+                {{ item.data_readings[6].value}}
+                </v-col>
+              </v-row>
+            </template>
+            <template v-slot:item.totalAmount="{ item }">
+                <v-row>
+                <v-col outline>
+                {{ item.data_readings[7].value}}
+                </v-col>
               </v-row>
             </template>
           </v-data-table-server>
@@ -63,13 +111,15 @@
   
   export default {
     data: () => ({
-      itemsPerPage: 10,
+      itemsPerPage: 40,
+      totalItems: 40
       headers: [
         {
           title: 'Date',
           align: 'start',
           sortable: false,
-          key: 'date',
+          // key: 'date',
+          value: 'date'
         },
         {
           title: 'Device Type',
@@ -94,11 +144,55 @@
         //   align: 'center',
         // }
         {
-          title: 'Readings',
-          value: 'readings',
+          title: 'Operator ID',
+          value: 'operators',
+          // key: 'device.station.name',
+          align: 'center',
+        },
+        {
+          title: 'Vehicle #',
+          value: 'vehicles',
+          sortable: true,
+          // key: 'device.station.name',
+          align: 'center',
+        },
+        {
+          title: 'Fuel Type',
+          value: 'fuel',
+          // key: 'device.station.name',
+          align: 'center',
+        },
+        {
+          title: 'Litres',
+          value: 'litres',
+          // key: 'device.station.name',
+          align: 'center',
+        },
+        {
+          title: 'Amount',
+          value: 'amount',
+          // key: 'device.station.name',
+          align: 'center',
+        },
+        {
+          title: 'Rate',
+          value: 'rate',
+          // key: 'device.station.name',
+          align: 'center',
+        },
+        {
+          title: 'Total\nLitres',
+          value: 'totalLitres',
+          // key: 'device.station.name',
+          align: 'center',
+        },
+        {
+          title: 'Total\nAmount',
+          value: 'totalAmount',
           // key: 'device.station.name',
           align: 'center',
         }
+
   
         // { title: 'Fat (g)', key: 'fat', align: 'end' },
         // { title: 'Carbs (g)', key: 'carbs', align: 'end' },
@@ -108,7 +202,7 @@
       search: '',
       serverItems: [],
       loading: true,
-      totalItems: 0,
+      totalItems: 40,
       dialog: false,
       selectedItem: {}
     }),
@@ -122,7 +216,7 @@
       return new Promise(resolve => {
         setTimeout(() => {
           const start = (page - 1) * itemsPerPage
-          const end = start + itemsPerPage
+          const end = start + itemsPerPage - 1
           //       console.log('actual data : ', data.data)
           const items = data.value.data.slice()
   
