7a1b025572c257f5ac91696c452c0baf0acdc1ae
Ahmed Qamar Mannan <ahmed.qamar@alliedc.com>
2024-10-27 04:17:37 +0500

    Removed logs.

--- server/api/login.post.ts
+++ server/api/login.post.ts
@@ -6,8 +6,6 @@ interface Params {
 export default defineEventHandler(async (event) => {
     const config = useRuntimeConfig()
     const { email, password } = await readBody<Params>(event)
-    console.log('login.post.ts > email : ', email)
-    console.log('login.post.ts > password : ', password)
     if (typeof email === 'string' && typeof password === 'string') {
         // if (email.length >= 6 && password.length >= 6) {
         const result = await $fetch(`${config.public.apiBaseURL}/api/token`, {
